60520a485226e8df83dfb456de70f5e9a61ccd8c
Ondrej Spišák <Ondrej Spik@LAPTOP-B6S3BR72>
2017-11-30 13:55:53 +0100

    Doplnenie este som upravil do popisovacie okno, aby to trocha lepsie vyzeralo

--- src/main/java/farma/ZvieraPopisController.java
+++ src/main/java/farma/ZvieraPopisController.java
@@ -8,7 +8,10 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
@@ -33,15 +36,52 @@ public class ZvieraPopisController {
     private TextFlow popisTextFlow;
 
     @FXML
-    private ListView<Zviera> vybraneZvieraListView;
+    private TableView<Zviera> vybraneZvieraTableView;
 
     @FXML
     void initialize() {
         Zviera zviera = zvieraDao.findByRegistracneCislo(kliknuteZviera.getRegistracneCislo());
         List<Zviera> vybraneZviera = new ArrayList<>();
-        vybraneZviera.add(zviera);
+        vybraneZviera.add(kliknuteZviera);
         zvierataList = FXCollections.observableArrayList(vybraneZviera);
-        vybraneZvieraListView.setItems(zvierataList);
+        
+        // registračné číslo
+        TableColumn<Zviera, String> registracneCisloCol = new TableColumn<>("Registračné číslo");
+        registracneCisloCol.setCellValueFactory(new PropertyValueFactory<>("registracneCislo"));
+        vybraneZvieraTableView.getColumns().add(registracneCisloCol);
+
+        // druh
+        TableColumn<Zviera, String> druhCol = new TableColumn<>("Druh");
+        druhCol.setCellValueFactory(new PropertyValueFactory<>("druh"));
+        vybraneZvieraTableView.getColumns().add(druhCol);
+
+        // plemeno
+        TableColumn<Zviera, String> plemenoCol = new TableColumn<>("Plemeno");
+        plemenoCol.setCellValueFactory(new PropertyValueFactory<>("plemeno"));
+        vybraneZvieraTableView.getColumns().add(plemenoCol);
+
+        // pohlavie
+        TableColumn<Zviera, String> pohlavieCol = new TableColumn<>("Pohlavie");
+        pohlavieCol.setCellValueFactory(new PropertyValueFactory<>("pohlavie"));
+        vybraneZvieraTableView.getColumns().add(pohlavieCol);
+
+        // dátum narodenia
+        TableColumn<Zviera, Object> datumNarodeniaCol = new TableColumn<>("Dátum narodenia");
+        datumNarodeniaCol.setCellValueFactory(new PropertyValueFactory<>("fDatumNarodenia"));
+        vybraneZvieraTableView.getColumns().add(datumNarodeniaCol);
+
+        // dátum nadobudnutia
+        TableColumn<Zviera, Object> datumNadobudnutiaCol = new TableColumn<>("Dátum nadobudnutia");
+        datumNadobudnutiaCol.setCellValueFactory(new PropertyValueFactory<>("fDatumNadobudnutia"));
+        vybraneZvieraTableView.getColumns().add(datumNadobudnutiaCol);
+
+        // kúpna cena
+        TableColumn<Zviera, Double> kupnaCenaCol = new TableColumn<>("Kúpna cena");
+        kupnaCenaCol.setCellValueFactory(new PropertyValueFactory<>("kupnaCena"));
+        vybraneZvieraTableView.getColumns().add(kupnaCenaCol);
+
+        vybraneZvieraTableView.setItems(FXCollections.observableArrayList(zvierataList));
+          
         popisZviera.setZviera(zviera);
         Text t1 = new Text(popisZviera.getPopis());
         popisTextFlow.getChildren().add(t1);
